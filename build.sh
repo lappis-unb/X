2d219e9db0a3aaac97eaca0d57270f2933c4a99c
Joenio Costa <joenio@joenio.me>
2019-10-19 13:48:02 -0300

    fix sh path, and use python3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 wget https://raw.githubusercontent.com/AlexandreDecan/genja/master/genja.py
 wget https://raw.githubusercontent.com/AlexandreDecan/genja/master/requirements.txt
-pip install -r requirements.txt
-python genja.py
+pip3 install -r requirements.txt
+python3 genja.py
